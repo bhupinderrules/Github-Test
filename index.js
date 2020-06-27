41cb738059b6210c92dd604a6faf971912751c15
Bhupinder Singh <bhupinderrules@gmail.com>
2020-06-27 16:37:35 +0530

    First working draft of the website
    First working draft of the website with atleast one functions of css and js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 let a = document.getElementById("span");
+let toggle = 0;
 a.addEventListener("click",colorInvert);
 function colorInvert(){
-    this.style.color = "#fda456";
+    if(toggle === 0){
+        this.style.color = "#fda456";
+        toggle = 1;
+    }
+    else{
+        this.style.color = "#231223";
+        toggle = 0;
+    }
 }
